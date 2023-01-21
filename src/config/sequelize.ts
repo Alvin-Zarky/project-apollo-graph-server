64846b5c8fd982c03d0afea23bfedb59f0a27bb9
Admin <user@MacBook-Pro.local>
2023-01-21 14:46:47 +0700

    graphql-server

---
+++ src/config/sequelize.ts
@@ -0,0 +1,9 @@
+import { Sequelize } from "sequelize";
+import {config} from "dotenv"
+
+config()
+const sequelize= new Sequelize(`${process.env.MYSQL_DEFAULT}`, {
+  host: `${process.env.HOST}`,
+  dialect: 'mysql'
+})
+export default sequelize
